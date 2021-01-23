616cf840dd0166431e28b321d76bcf6ab268d655
Tiago Soares <fullcoderb@gmail.com>
2021-01-23 14:08:14 -0300

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Fala Dev!')
-})
+
+    const {name, email} = req.query;
+
+    let data = {
+        message: `Fala ${name}! do email ${email}`,
+    };
+
+    res.json(data);
+});
 
 app.listen(3333, () => {
     console.log('App is runing on port 3333');
