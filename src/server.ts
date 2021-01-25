d91b3ff6c017b04c81648879b016cdeb22610227
Tiago Soares <fullcoderb@gmail.com>
2021-01-25 20:46:15 -0300

    add ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,10 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.json({
-        message: "Fala Dev",
+        message: "Fala Go Stack s2",
     })
 })
 
 app.listen(3333, () =>{
-    console.log('App is running on port 3333');
+    console.log('App started on port 3333');
 });
