b4d4fcdc4bdc8c9e3cd8fa5e8de4d67913b5e772
Tiago Soares <fullcoderb@gmail.com>
2021-01-29 00:36:09 -0300

    body parser + interface appintments + verify date book

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,12 +1,49 @@
 import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.post('/', (req, res) => {
+interface Appoitment{
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments:Appoitment[] = [];
+
+appointmentsRouter.get('/', (request, response) => {
+
+    return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+
+    const {provider, date} = request.body;
+
+    const parserdDate = startOfHour(parseISO(date));
+
+    const findAppintmentInSameDate = appointments.find( appointment => 
+        isEqual(parserdDate, appointment.date)
+    );
+
+    console.log(findAppintmentInSameDate);
+
+    if(findAppintmentInSameDate){
+        return response.status(400).json({
+            message: 'This appointment is already booked',
+        });
+    }
     
-    return res.json({
-        message: "post appointments"}
-        );
-})
+    const appointment = {
+        id: uuid(),
+        provider,
+        date: parserdDate
+    };
+    
+    appointments.push(appointment);
+
+    return response.json(appointment);
+});
 
 export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import routes from './routes';
+import parser from 'body-parser';
+import cors from 'cors';
+
 const app = express();
 
+app.use(cors());
+app.use(parser.json());
 app.use(routes);
 
 app.listen(3333, () =>{
